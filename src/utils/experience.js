9efaa44fb9cdfbc2269e809d548454f046be98d0
Enmanuel Jarquín <enmisac@gmail.com>
2023-08-25 16:06:45 -0600

    added basic experienc section

---
+++ src/utils/experience.js
@@ -0,0 +1,255 @@
+export default  [
+    {
+        company: 'NewFire Global Partners',
+        link: "https://www.newfireglobal.com/",
+        role: "Full- Stack Engineer",
+        notes: [],
+        projects: [
+            {
+                name: "Health Data Analytics Institute (HDAI)",
+                link: "https://www.hda-institute.com/",
+                description: "Full-stack application related to the healthcare industry to optimize the way how clinics care for their patients and help predict and prevent future diseases.",
+                mainTasks: [
+                    {
+                        description: "Maintain and create new features in the backed app built on top of the NestJS framework and improve a Dashboard built with React.",
+                        link: "",
+                    },
+                    {
+                        description: "Improve the frontend workflow with Docker.",
+                        link: ""
+                    },
+                    {
+                        description: "Help to implement third-party libraries such as React-Query and Vite.",
+                        link: ""
+                    }
+                ],
+                majorContributions: [
+                    {
+                        description: "Proposed two libraries (turborepo and react-query) to improve our workflow, that impacted directly our delivery time, we passed of modifying two or four request in a single page, to create a single request and besides cache data in the frontend.",
+                        link: "I configured the app to support google maps API."
+                    },
+                    {
+                        description: "I configured the app to support google maps API.",
+                        link: ""
+                    }
+                ],
+                stack: "NestJS,ReactJS,Liquibase,MySQL,Google Maps,Ant-D,Vite,React-query,AWS" 
+            },
+            {
+                name: "TCare",
+                link: "https://www.tcare.ai/",
+                description: "Full-stack application related to the healthcare industry to optimize the way how clinics care for their patients and help predict and prevent future diseases.",
+                mainTasks: [
+                    {
+                        description: "Create and configure a project from scratch using the client preference tools, in this case, Svelte and Svelte Kit.",
+                        link: ""
+                    },
+                    {
+                        description: "Create components from scratch using Storybook",
+                        link: ""
+                    },
+                    {
+                        description: "Review PRs",
+                        lint: ""
+                    },
+                    {
+                        description: "Implement designs made in Figmas",
+                        lint: ""
+                    },
+                    {
+                        description: "Translate part of the content into Spanish",
+                        link: ""
+                    },
+                    {
+                        description: "Propose and implement new tools to improve our workflow",
+                        link: ""
+                    }
+                ],
+                majorContributions: [
+                    {
+                        description: "Created a core component that is being used across the entire app. Parts of my components are being used in this page https://together-dev.mytcare.ai/",
+                        link: "https://together-dev.mytcare.ai/"
+                    },
+                    {
+                        description: "Helped to define workflow along with Designers and QA",
+                        link: ""
+                    },
+                    {
+                        description: "Integrate external tools to improve our workflow",
+                        link: ""
+                    },
+                    {
+                        description: "Configure the multi-language feature",
+                        link: ""
+                    },
+                    {
+                        description: "Translate part of the content into Spanish",
+                        link: ""
+                    }
+                ],
+                stack: "Svelte,Svelte Kit,Storybook,Svelte-Query,Skeleton UI,GitHub,Jira" 
+            }
+        ]
+    },
+    {
+        company: 'Deviget',
+        role: "Full-Stack NodeJS/ReactJS Engineer",
+        link: "https://www.deviget.com/",
+        notes: [],
+        projects: [
+            {
+                name: "Impulsum VC",
+                link: "https://impulsum.vc/",
+                description: "Create dynamic webpages with react components based on the data defined on Contentful CMS.",
+                mainTasks: [
+                    {
+                        description: "I oversaw maintaining and supporting an in-house library with Storybook and Chromatic",
+                        link: ""
+                    },
+                    {
+                        description: "Create components from scratch using Storybook and Figma",
+                        link: ""
+                    },
+                    {
+                        description: "Review PRs",
+                        link: ""
+                    },
+                    {
+                        description: "Implement designs made in Figma",
+                        link: ""
+                    },
+                ],
+                majorContributions: [
+                    {
+                        description: "Implement the business logic to create react components dynamically based on a specific format, which was stored in a JSON file inside of Contenful.",
+                        link: ""
+                    },   
+                    {
+                        description: "Create our main landing page following the approach described above.",
+                        link: ""
+                    },    
+                    {
+                        description: "This page https://cmpsr.io/ was created by myself, using dynamic components with React, NextJS, and Contentful.",
+                        link: ""
+                    }
+                ],
+                stack: "NextJS,ReactJS,GraphQL,Contentful,Storybook,Lerna,Github,Mono repositories,Chacka UI" 
+            }
+        ]
+    },
+    {
+        company: 'Applaudo Studios',
+        role: "Full Stack Developer",
+        link: "https://applaudostudios.com/",
+        notes: [],
+        projects: [
+            {
+                name: "Lightspeed Systems",
+                link: "https://www.lightspeedsystems.com/",
+                description: "Extracting data from AWS services via the serverless app and populating a frontend dashboard built on top of ReactJS.",
+                mainTasks: [
+                    {
+                        description: "Implement unit tests and integration tests",
+                        link: ""
+                    },
+                    {
+                        description: "Creating custom libraries",
+                        link: ""
+                    },
+                    {
+                        description: "Create new features on the frontend side for the main platform which was a Dashboard (tables, charts, etc)",
+                        link: ""
+                    },
+                    {
+                        description: "Create new features on the backend side to process, send and get data from the AWS services",
+                        link: ""
+                    },
+                ],
+                majorContributions: [
+                    {
+                        description: "Added more over 80% of our unit tests, I implemented, configured the testing libraries and added most test in a team of 6 developers.",
+                        link: ""
+                    },
+                    {
+                        description: "I did extra work to be able to get 2 important pages that contained dashboards.",
+                        link: ""
+                    },
+                ],
+                stack: "NestJS,ReactJS,Storybook,AWS services,DynamoDB,Lambda,API Gateway,Recharts" 
+            }
+        ]
+    },
+    {
+        company: 'IT Crowd Argentina',
+        role: "Full Stack Developer",
+        link: "https://www.itcrowdarg.com/",
+        notes: [
+            {
+                description: "I participated in other projects reviewing code, mentoring, and from time to time supporting Angular and ReactJS projects along with Typescript.",
+                link: ""
+            },
+            {
+                description: "After one year in the company, I started helping with technical interviews, defining the tech interview format, and conducting the interviews themselves.",
+                link: ""
+            }
+        ],
+        projects: [
+            {
+                name: "Mitrol",
+                link: "https://www.mitrol.net/",
+                description: "As a full-stack developer I was in charge of creating and maintaining a real-time platform for clients from Argentina.",
+                mainTasks: [
+                    {
+                        description: "I created a server with NodeJS to receive and send data in real-time using Kafka.",
+                        link: ""
+                    },
+                    {
+                        description: "I created a library to manage the session with NodeJS and Redis.",
+                        link: ""
+                    },
+                    {
+                        description: "Modified a Kafka library to connect with NodeJS.",
+                        link: ""
+                    },
+                    {
+                        description: "Created a UI library with ReactJS and published it in NPM",
+                        link: ""
+                    },
+                ],
+                majorContributions: [
+                    {
+                        description: "I help a lot with a service connection with Kafka and two servers to send data in real-time via graphql to a dashboard with ReactJS.",
+                        link: ""
+                    },
+                ],
+                stack: "NodeJS,GraphQL,ReactJS,Kafka,Storybook,GitHub,Bitbucket,Docker,JIRA" 
+            },
+            {
+                name: "Storydots",
+                link: "https://www.storydots.app/",
+                description: "I created the MVP of the Storydots company, this company was born within IT Crowd, and I was in charge of creating the first version of it to be able to attach video greetings to gifts or physical purchases.",
+                mainTasks: [
+                    {
+                        description: "Create an app from scratch to scan QR codes, record a greeting (using the camera) and send it to AWS https://storydots.app/",
+                        link: "https://storydots.app/"
+                    },
+                    {
+                        description: "Records videos via device camera",
+                        link: ""
+                    },
+                    {
+                        description: "Send files to AWS S3",
+                        link: ""
+                    }
+                ],
+                majorContributions: [
+                    {
+                        description: "Create the MVP of what today is a great startup in Argentina.",
+                        link: ""
+                    }
+                ],
+                stack: "ReactJS,Storybook,Video HTML API,AWS S3,Lambdas."
+            }
+        ]
+    },
+]
