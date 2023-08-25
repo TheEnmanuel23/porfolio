11c60e31d9de5b9b1069b072864d908b18f3d954
Enmanuel Jarquín <enmisac@gmail.com>
2023-08-24 23:55:15 -0600

    add alpinejs to deal with accordion feature, and upgrade libraries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx(), image()],
-	experimental: {
-		assets: true,
-	},
+  integrations: [tailwind(), mdx(), alpinejs()],
+  experimental: {
+    assets: true
+  }
 });
