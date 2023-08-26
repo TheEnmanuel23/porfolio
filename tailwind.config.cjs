24af17d135065bfd2f56fac687ac5707861e10b6
Enmanuel Jarquín <enmisac@gmail.com>
2023-08-26 00:07:37 -0600

    disabled dark theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,6 @@ const plugin = require('tailwindcss/plugin');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
 	theme: {
 		screens: {
 			xs: '480px',
